3ee7a96329ba1ac4e946edf008516a25240f2aa4
aaronpoweruser <atlrudeboi1@gmail.com>
2013-02-23 09:02:42 -0800

    Update to supersu 1.10 multiuser edition
    Change-Id: I80142679ed62a5d0ab1d4a970b0766672138815f

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -9,7 +9,8 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk \
     vendor/pa/prebuilt/common/apk/SuperSU.apk:system/app/SuperSU.apk \
-    vendor/pa/prebuilt/common/xbin/su:system/xbin/su
+    vendor/pa/prebuilt/common/xbin/su:system/xbin/su \
+    vendor/pa/prebuilt/common/bin/.ext/.su:system/bib/.ext/.su
 
 # Exclude prebuilt paprefs from builds if the flag is set
 ifneq ($(PREFS_FROM_SOURCE),true)
